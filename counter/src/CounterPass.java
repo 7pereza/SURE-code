b049bfd1ed8494b6c029f7c8848a5c7cc76cff25
Adrian Perez <adrianjperez@lewisu.edu>
2020-07-16 13:03:12 -0500

    add new folder with code

---
+++ counter/src/CounterPass.java
@@ -0,0 +1,32 @@
+/* this is to be use in the server do java Counterpass (file name )
+ * making this java program to help me counter the number of password in the file
+ * 
+ * 
+ * i will later,  if i need to make this a menu driven program
+ *  */
+import java.util.Scanner;
+import java.io.*;
+
+
+public class CounterPass {
+
+	public static void main(String[] args) {		
+		
+		try {
+			Scanner sc = new Scanner(new BufferedReader(new FileReader(new File (args[0]) ) ) );
+			int count = 0;
+			
+			while(sc.hasNextLine()) {
+				sc.nextLine();
+				count++;
+			}
+			
+			System.out.println(args[0] + " has..." + count);
+		}catch(Exception e) {
+			System.out.println("Something happen when reading the file");
+			e.printStackTrace();
+		}//end if try block		
+
+	}//end of main
+
+}//end of class
