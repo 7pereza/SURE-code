bc78fddc7b616a1f1ff56b42a419ff66040960b3
Adrian Perez <adrianjperez@lewisu.edu>
2020-07-20 16:23:49 -0500

    edit output format

--- com/src/CountMatches.java
+++ com/src/CountMatches.java
@@ -4,7 +4,8 @@
  * to count. <passcompare> has a total of three while loops that it is going to use .
  * there is a while loop that has a while loop inside of it and that is making it take longer 
  * 
- * 
+ * To use this just give the file name after calling the java command 
+ * example) java CountMatches <FileName>
  * */
 
 import java.util.Scanner;
@@ -18,6 +19,8 @@ public class CountMatches {
 		int counter = 1; 
 		
 		try {
+			
+			
 			Scanner file1 = new Scanner ( new BufferedReader( new FileReader( new File ( args[0]) ) ) );
 			Scanner file2 = new Scanner ( new BufferedReader( new FileReader( new File ( args[0]) ) ) );
 			String str1 = "";
@@ -26,6 +29,7 @@ public class CountMatches {
 			
 			file2.nextLine();//make this one step ahead then file1 scanner
 			
+			System.out.println("Starting to count file....");
 			while(file2.hasNextLine()) {
 				str1 = file1.next();//get the first word
 				str2 = file2.next();
@@ -42,11 +46,13 @@ public class CountMatches {
 			file2.close();			
 			
 		} catch (Exception e) {	
-			System.out.println("Something happen when counting");
-			e.printStackTrace();
+			System.out.println("Something happen when counting !?!?!?!?!!");
+			//e.printStackTrace();
 		}//end of try block		
+		
 		System.out.println("File name..." + args[0]);
 		System.out.println("\nThe number of matchs: " + counter);
+		System.out.println("--------------------------------------------------------------------");
 		
 		
 	}//end of function
